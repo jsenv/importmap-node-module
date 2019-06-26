d29d90b7130f4a4a7599ea56e0601c6e173d1180
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-26 15:32:58 +0200

    add main jsnext test

---
+++ test/main-jsnext/main-jsnext.test.js
@@ -0,0 +1,18 @@
+import { importMetaURLToFolderPath } from "@jsenv/operating-system-path"
+import { assert } from "@dmail/assert"
+import { generateImportMapForProjectNodeModules } from "../../index.js"
+
+const testFolderPath = importMetaURLToFolderPath(import.meta.url)
+const actual = await generateImportMapForProjectNodeModules({
+  projectPath: testFolderPath,
+  writeImportMapFile: false,
+  scopeOriginRelativePerModule: false,
+  remapFolder: false,
+})
+const expected = {
+  imports: {
+    "main-jsnext": "/node_modules/main-jsnext/jsnext.js",
+  },
+  scopes: {},
+}
+assert({ actual, expected })
