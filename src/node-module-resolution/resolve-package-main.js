365f093b144cb712bb119f18bab9b264cdcacb85
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-26 15:29:47 +0200

    add main undefined test

--- src/node-module-resolution/resolve-package-main.js
+++ src/node-module-resolution/resolve-package-main.js
@@ -2,27 +2,30 @@ import { extname } from "path"
 import { stat } from "fs"
 import { firstOperationMatching } from "@dmail/helper"
 import { pathnameToOperatingSystemPath } from "@jsenv/operating-system-path"
+import { pathnameToDirname } from "@jsenv/module-resolution"
 
 export const resolvePackageMain = (packageData, packagePathname) => {
   if ("module" in packageData) return resolveMainFile(packageData.module, packagePathname)
   if ("jsnext:main" in packageData)
     return resolveMainFile(packageData["jsnext:main"], packagePathname)
   if ("main" in packageData) return resolveMainFile(packageData.main, packagePathname)
-  return resolveMainFile("index.js", packagePathname)
+  return resolveMainFile("index", packagePathname)
 }
 
 const extensionCandidateArray = ["js", "json", "node"]
 
 const resolveMainFile = async (main, packagePathname) => {
   if (main.slice(0, 2) === "./") main = main.slice(2)
+  if (main[0] === "/") main = main.slice(1)
 
   const extension = extname(main)
   if (extension === "") {
     const extensionLeadingToFile = await firstOperationMatching({
       array: extensionCandidateArray,
       start: async (extensionCandidate) => {
+        const packageDirname = pathnameToDirname(packagePathname)
         const path = pathnameToOperatingSystemPath(
-          `${packagePathname}/${main}.${extensionCandidate}`,
+          `${packageDirname}/${main}.${extensionCandidate}`,
         )
         const isFile = await pathLeadsToAFile(path)
         return isFile ? extensionCandidate : null
