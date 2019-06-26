43b96d53a3bea7b7356f282eace57881a6cb1ef9
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-26 15:31:40 +0200

    add main relative test

---
+++ test/main-relative/main-relative.test.js
@@ -0,0 +1,18 @@
+import { importMetaURLToFolderPath } from "@jsenv/operating-system-path"
+import { assert } from "@dmail/assert"
+import { generateImportMapForProjectNodeModules } from "../../index.js"
+
+const testFolderPath = importMetaURLToFolderPath(import.meta.url)
+const actual = await generateImportMapForProjectNodeModules({
+  projectPath: testFolderPath,
+  writeImportMapFile: false,
+  scopeOriginRelativePerModule: false,
+  remapFolder: false,
+})
+const expected = {
+  imports: {
+    "main-relative": "/node_modules/main-relative/lib/index.js",
+  },
+  scopes: {},
+}
+assert({ actual, expected })
