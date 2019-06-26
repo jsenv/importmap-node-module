c5dfe77386faba9b0188af8802302d2701f633da
Damien Maillard <damien.maillard@dailymotion.com>
2019-06-26 12:51:38 +0200

    move sort outof main file

--- src/generateImportMapForProjectNodeModules.js
+++ src/generateImportMapForProjectNodeModules.js
@@ -11,6 +11,7 @@ import { readPackageData } from "./readPackageData.js"
 import { resolveNodeModule } from "./resolveNodeModule.js"
 import { resolvePackageMain } from "./package-main.js"
 import { DEFAULT_IMPORT_MAP_RELATIVE_PATH } from "./generate-import-map-constant.js"
+import { sortImportMap } from "./sort.js"
 
 export const generateImportMapForProjectNodeModules = async ({
   projectPath,
@@ -218,64 +219,6 @@ export const generateImportMapForProjectNodeModules = async ({
       })
     })
   })
-
-const sortImportMap = (importMap) => {
-  const orderedImportMap = {
-    imports: sortImportMapImports(importMap.imports),
-    scopes: sortImportMapScopes(importMap.scopes),
-  }
-  return orderedImportMap
-}
-
-const sortImportMapImports = (imports) => {
-  const sortedImports = {}
-  Object.keys(imports)
-    .sort(compareLengthOrLocaleCompare)
-    .forEach((name) => {
-      sortedImports[name] = imports[name]
-    })
-  return sortedImports
-}
-
-const compareLengthOrLocaleCompare = (a, b) => {
-  return b.length - a.length || a.localeCompare(b)
-}
-
-const sortImportMapScopes = (scopes) => {
-  const sortedScopes = {}
-  Object.keys(scopes)
-    .sort(compareLengthOrLocaleCompare)
-    .forEach((scopeName) => {
-      sortedScopes[scopeName] = sortScopedImports(scopes[scopeName], scopeName)
-    })
-  return sortedScopes
-}
-
-const sortScopedImports = (scopedImports) => {
-  const compareScopedImport = (a, b) => {
-    // const aIsRoot = a === "/"
-    // const bIsRoot = b === "/"
-    // if (aIsRoot && !bIsRoot) return 1
-    // if (!aIsRoot && bIsRoot) return -1
-    // if (aIsRoot && bIsRoot) return 0
-
-    // const aIsScope = a === scope
-    // const bIsScope = b === scope
-    // if (aIsScope && !bIsScope) return 1
-    // if (!aIsScope && bIsScope) return -1
-    // if (aIsScope && bIsScope) return 0
-
-    return compareLengthOrLocaleCompare(a, b)
-  }
-
-  const sortedScopedImports = {}
-  Object.keys(scopedImports)
-    .sort(compareScopedImport)
-    .forEach((name) => {
-      sortedScopedImports[name] = scopedImports[name]
-    })
-  return sortedScopedImports
-}
 
 const createNodeModuleNotFoundMessage = ({
   projectPath,
