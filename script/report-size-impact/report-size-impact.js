4e2ad13c90481053593799a51f9a54287b8a17f8
Damien Maillard <damien.maillard@dailymotion.com>
2019-10-23 17:53:07 +0200

    fix eslint failures

--- script/report-size-impact/report-size-impact.js
+++
@@ -1,6 +0,0 @@
-const { reportSizeImpactIntoGithubPullRequest } = require("@jsenv/continuous-size-reporting")
-const { projectPath } = require("../../jsenv.config.js")
-
-reportSizeImpactIntoGithubPullRequest({
-  projectPath,
-})
