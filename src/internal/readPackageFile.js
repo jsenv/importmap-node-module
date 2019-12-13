ed98edd2f165f078d9a19b720c5773db6e242bf3
Damien Maillard <damien.maillard@dailymotion.com>
2019-12-13 16:23:12 +0100

    add manual override concept

--- src/internal/readPackageFile.js
+++ src/internal/readPackageFile.js
@@ -1,10 +1,41 @@
 import { promisify } from "util"
 import { readFile } from "fs"
 
-export const readPackageFile = async (path) => {
+export const readPackageFile = async (path, manualOverrides) => {
   const packageFileString = await readFileContent(path)
   const packageJsonObject = JSON.parse(packageFileString)
+  const { name, version } = packageJsonObject
+
+  const overrideKey = Object.keys(manualOverrides).find((overrideKeyCandidate) => {
+    if (name === overrideKeyCandidate) return true
+    if (`${name}@${version}` === overrideKeyCandidate) return true
+    return false
+  })
+  if (overrideKey) {
+    return composeObject(packageJsonObject, manualOverrides[overrideKey])
+  }
   return packageJsonObject
+}
+
+const composeObject = (leftObject, rightObject) => {
+  const composedObject = {
+    ...leftObject,
+  }
+  Object.keys(rightObject).forEach((key) => {
+    const rightValue = rightObject[key]
+
+    if (rightValue === null || typeof rightValue !== "object" || key in leftObject === false) {
+      composedObject[key] = rightValue
+    } else {
+      const leftValue = leftObject[key]
+      if (leftValue === null || typeof leftValue !== "object") {
+        composedObject[key] = rightValue
+      } else {
+        composedObject[key] = composeObject(leftValue, rightValue)
+      }
+    }
+  })
+  return composedObject
 }
 
 const readFilePromisified = promisify(readFile)
