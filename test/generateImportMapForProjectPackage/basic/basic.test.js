e7d3022dc58c5339a4395c27425f5512896f1407
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-03 14:23:58 +0100

    fix tests, also this pr will fix #9

--- test/generateImportMapForProjectPackage/basic/basic.test.js
+++ test/generateImportMapForProjectPackage/basic/basic.test.js
@@ -10,13 +10,16 @@ const expected = {
   imports: {
     "@dmail/yo/": "./node_modules/@dmail/yo/",
     "@dmail/yo": "./node_modules/@dmail/yo/index.js",
-    "whatever/": "./",
+    "root/": "./",
     "bar/": "./node_modules/bar/",
     "foo/": "./node_modules/foo/",
     "bar": "./node_modules/bar/bar.js",
     "foo": "./node_modules/foo/foo.js",
   },
   scopes: {
+    "./node_modules/foo/node_modules/bar/": {
+      "bar/": "./node_modules/foo/node_modules/bar/",
+    },
     "./node_modules/foo/": {
       bar: "./node_modules/foo/node_modules/bar/index.js",
     },
