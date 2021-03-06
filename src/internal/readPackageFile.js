0c1638a6e6d92dfcea98ef8b3de290f4b8a17dad
Damien Maillard <damien.maillard@dailymotion.com>
2021-03-06 11:04:15 +0100

    Create a file for applyPackageManualOverride

--- src/internal/readPackageFile.js
+++ src/internal/readPackageFile.js
@@ -1,38 +1,8 @@
 import { readFile } from "@jsenv/util"
+import { applyPackageManualOverride } from "./applyPackageManualOverride.js"
 
 export const readPackageFile = async (packageFileUrl, packagesManualOverrides) => {
   const packageFileString = await readFile(packageFileUrl)
   const packageJsonObject = JSON.parse(packageFileString)
-  const { name, version } = packageJsonObject
-
-  const overrideKey = Object.keys(packagesManualOverrides).find((overrideKeyCandidate) => {
-    if (name === overrideKeyCandidate) return true
-    if (`${name}@${version}` === overrideKeyCandidate) return true
-    return false
-  })
-  if (overrideKey) {
-    return composeObject(packageJsonObject, packagesManualOverrides[overrideKey])
-  }
-  return packageJsonObject
-}
-
-const composeObject = (leftObject, rightObject) => {
-  const composedObject = {
-    ...leftObject,
-  }
-  Object.keys(rightObject).forEach((key) => {
-    const rightValue = rightObject[key]
-
-    if (rightValue === null || typeof rightValue !== "object" || key in leftObject === false) {
-      composedObject[key] = rightValue
-    } else {
-      const leftValue = leftObject[key]
-      if (leftValue === null || typeof leftValue !== "object") {
-        composedObject[key] = rightValue
-      } else {
-        composedObject[key] = composeObject(leftValue, rightValue)
-      }
-    }
-  })
-  return composedObject
+  return applyPackageManualOverride(packageJsonObject, packagesManualOverrides)
 }
