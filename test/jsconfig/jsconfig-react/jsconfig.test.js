cc2cec158719271b217e38501919b95cdf30052c
Damien Maillard <damien.maillard@dailymotion.com>
2021-04-11 15:10:47 +0200

    add test to ensure how jsconfig file is written

---
+++ test/jsconfig/jsconfig-react/jsconfig.test.js
@@ -0,0 +1,54 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl, readFile, writeFile, removeFileSystemNode } from "@jsenv/util"
+import { writeImportMapFile } from "@jsenv/node-module-import-map"
+
+const testDirectoryUrl = resolveUrl("./", import.meta.url)
+const jsConfigFileUrl = resolveUrl("jsconfig.json", testDirectoryUrl)
+
+// arrange
+await writeFile(
+  jsConfigFileUrl,
+  JSON.stringify(
+    {
+      compilerOptions: {
+        jsx: "react",
+        paths: {
+          "src/*": ["./src/*"],
+        },
+      },
+    },
+    null,
+    "  ",
+  ),
+)
+// act
+await writeImportMapFile(
+  [
+    {
+      imports: { foo: "./bar.js" },
+    },
+  ],
+  {
+    projectDirectoryUrl: testDirectoryUrl,
+    importMapFile: false,
+    jsConfigFile: true,
+  },
+)
+// assert
+try {
+  const actual = await readFile(jsConfigFileUrl, { as: "json" })
+  const expected = {
+    compilerOptions: {
+      baseUrl: ".",
+      // paths are overwritten
+      paths: {
+        foo: ["./bar.js"],
+      },
+      // react is kept
+      jsx: "react",
+    },
+  }
+  assert({ actual, expected })
+} finally {
+  await removeFileSystemNode(jsConfigFileUrl)
+}
