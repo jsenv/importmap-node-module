735a010be14c9da9c941cecac9d57be879b311ee
Damien Maillard <damien.maillard@dailymotion.com>
2021-05-20 11:05:31 +0200

    update basic test

--- test/getImportMapFromProjectFiles/core/basic/basic.test.js
+++ test/getImportMapFromProjectFiles/core/basic/basic.test.js
@@ -1,5 +1,6 @@
 import { assert } from "@jsenv/assert"
 import { resolveUrl } from "@jsenv/util"
+
 import { getImportMapFromProjectFiles } from "@jsenv/node-module-import-map"
 
 const testDirectoryUrl = resolveUrl("./root/", import.meta.url)
