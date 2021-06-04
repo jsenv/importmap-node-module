4af4d26c60151a9497d0b7368aceb7d7d9a3ecfb
Damien Maillard <damien.maillard@dailymotion.com>
2021-06-04 11:29:25 +0200

    write a failing test case for exports not specified

---
+++ test/getImportMapFromProjectFiles/package-exports-field/exports-not-specified/exports-not-specified.test.js
@@ -0,0 +1,22 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl } from "@jsenv/util"
+
+import { getImportMapFromProjectFiles } from "@jsenv/node-module-import-map"
+
+const testDirectoryUrl = resolveUrl("./root/", import.meta.url)
+
+const actual = await getImportMapFromProjectFiles({
+  projectDirectoryUrl: testDirectoryUrl,
+  jsFiles: true,
+})
+const expected = {
+  imports: {
+    whatever: "./index.js",
+  },
+  scopes: {
+    "./node_modules/foo/": {
+      "foo/": "./node_modules/foo/",
+    },
+  },
+}
+assert({ actual, expected })
