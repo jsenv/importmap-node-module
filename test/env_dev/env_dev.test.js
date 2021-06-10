7a5a7873d05b5a0dbf2e501b2e2988e6f820a6e8
Damien Maillard <damien.maillard@dailymotion.com>
2021-06-10 10:21:08 +0200

    Add test about #env remapping

---
+++ test/env_dev/env_dev.test.js
@@ -0,0 +1,35 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl } from "@jsenv/util"
+
+import { getImportMapFromProjectFiles } from "@jsenv/node-module-import-map"
+
+const testDirectoryUrl = resolveUrl("./root/", import.meta.url)
+
+const warnings = []
+const importmap = await getImportMapFromProjectFiles({
+  projectDirectoryUrl: testDirectoryUrl,
+  jsFiles: true,
+  initialImportMap: {
+    imports: {
+      "#env": "./env.dev.js",
+    },
+  },
+  onWarn: (warning) => {
+    warnings.push(warning)
+  },
+})
+const actual = {
+  warnings,
+  importmap,
+}
+const expected = {
+  warnings: [],
+  importmap: {
+    imports: {
+      "whatever": "./index.js",
+      "#env": "./env.dev.js",
+    },
+    scopes: {},
+  },
+}
+assert({ actual, expected })
