fd6611014709732a5cff69aec0409be15bc6440b
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-04 09:24:43 +0200

    update many tests

---
+++ test/package_symlink/package_symlink.test.mjs
@@ -0,0 +1,32 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl } from "@jsenv/filesystem"
+
+import { writeImportMapFiles } from "@jsenv/importmap-node-module"
+
+const testDirectoryUrl = resolveUrl("./root/", import.meta.url)
+
+const importmaps = await writeImportMapFiles({
+  projectDirectoryUrl: testDirectoryUrl,
+  importMapFiles: {
+    "test.importmap": {
+      mappingsForNodeResolution: true,
+      mappingsTreeshaking: true,
+    },
+  },
+  writeFiles: false,
+})
+
+const actual = importmaps["test.importmap"]
+const expected = {
+  imports: {
+    root: "./index.js",
+    foo: "./node_modules/foo/index.js",
+  },
+  scopes: {
+    "./node_modules/foo/": {
+      "./node_modules/foo/": "./node_modules/foo/",
+      "foo/": "./node_modules/foo/",
+    },
+  },
+}
+assert({ actual, expected })
