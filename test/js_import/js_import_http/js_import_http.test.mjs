ddd33f97ba32284ecb3df09b007647b763c3e93b
Damien Maillard <damien.maillard@dailymotion.com>
2021-09-06 08:49:45 +0200

    reorg test files

---
+++ test/js_import/js_import_http/js_import_http.test.mjs
@@ -0,0 +1,71 @@
+import { assert } from "@jsenv/assert"
+import { resolveUrl } from "@jsenv/filesystem"
+
+import { writeImportMapFiles } from "@jsenv/importmap-node-module"
+
+const testDirectoryUrl = resolveUrl("./root/", import.meta.url)
+
+const test = async ({ runtime } = {}) => {
+  const warnings = []
+  const importmaps = await writeImportMapFiles({
+    projectDirectoryUrl: testDirectoryUrl,
+    importMapFiles: {
+      "test.importmap": {
+        mappingsForNodeResolution: true,
+        removeUnusedMappings: true,
+        runtime,
+      },
+    },
+    onWarn: (warning) => {
+      warnings.push(warning)
+    },
+    writeFiles: false,
+  })
+  return { warnings, importmaps }
+}
+
+{
+  const actual = await test()
+  const expected = {
+    warnings: [
+      {
+        code: "IMPORT_RESOLUTION_FAILED",
+        message: `Import resolution failed for "fs"
+--- import source ---
+${testDirectoryUrl}index.js:1:7
+> 1 | import "fs"
+    |       ^
+  2 |${" "}
+--- reason ---
+there is no mapping for this bare specifier
+--- suggestion 1 ---
+use runtime: "node"`,
+      },
+    ],
+    importmaps: {
+      "test.importmap": {
+        imports: {
+          root: "./index.js",
+        },
+        scopes: {},
+      },
+    },
+  }
+  assert({ actual, expected })
+}
+
+{
+  const actual = await test({ runtime: "node" })
+  const expected = {
+    warnings: [],
+    importmaps: {
+      "test.importmap": {
+        imports: {
+          root: "./index.js",
+        },
+        scopes: {},
+      },
+    },
+  }
+  assert({ actual, expected })
+}
