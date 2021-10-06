8d2fe3a211b40b246b2f9b3ac5b8aa3b5fbd2e0d
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-06 09:56:06 +0200

    update some dev files

--- jsenv.config.mjs
+++ jsenv.config.mjs
@@ -1,1 +1,10 @@
+/*
+ * This file exports configuration reused by jsenv scripts such as
+ *
+ * script/test/test.mjs
+ * script/build/build.mjs
+ *
+ * Read more at https://github.com/jsenv/jsenv-core#jsenvconfigmjs
+ */
+
 export const projectDirectoryUrl = String(new URL("./", import.meta.url))
