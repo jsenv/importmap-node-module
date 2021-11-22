d6df2e1e102a2afac342e5e500342cde820b0647
Damien Maillard <damien.maillard@dailymotion.com>
2021-11-22 14:10:23 +0100

    remove prettier scripts

--- script/prettier/prettier_format.mjs
+++
@@ -1,7 +0,0 @@
-import { formatWithPrettier } from "@jsenv/prettier-check-project"
-
-import { projectDirectoryUrl } from "../../jsenv.config.mjs"
-
-await formatWithPrettier({
-  projectDirectoryUrl,
-})
