c89eb8b70c41da36d4521dd8be6e700923f0f49c
dmail <dmaillard06@gmail.com>
2021-12-13 18:17:20 +0100

    remove cjs build

--- jsenv.config.mjs
+++ jsenv.config.mjs
@@ -2,7 +2,6 @@
  * This file exports configuration reused by jsenv scripts such as
  *
  * script/test/test.mjs
- * script/build/build.mjs
  *
  * Read more at https://github.com/jsenv/jsenv-core#jsenvconfigmjs
  */
