a93d5dc73ce0ff409dff9c812f3718884275bb1a
dmail <dmaillard06@gmail.com>
2023-10-19 11:12:06 +0200

    update deps

--- experiment/lit_typescript/dist/index.js
+++ experiment/lit_typescript/dist/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { html, css } from "lit";
 import { customElement, property } from "lit/decorators.js";
-console.log({ html: html, css: css, customElement: customElement, property: property });
+console.log({
+  html: html,
+  css: css,
+  customElement: customElement,
+  property: property,
+});
