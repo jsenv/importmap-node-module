fbfa9ff3a3504a5f5ba8e1c87dd79955b5a71406
dmail <dmaillard06@gmail.com>
2023-11-28 15:36:56 +0100

    update test for importmap inside html

---
+++ tests/write_inside_html/write_inside_html.test.mjs
@@ -0,0 +1,36 @@
+import { takeDirectorySnapshot } from "@jsenv/snapshot";
+import { copyFileSync } from "@jsenv/filesystem";
+
+import { writeImportmaps } from "@jsenv/importmap-node-module";
+
+const testDirectoryUrl = new URL("./root/", import.meta.url);
+const snapshotDirectoryUrl = new URL("./snapshots/", import.meta.url);
+
+const test = async (fixtureName) => {
+  copyFileSync({
+    from: new URL(`./fixtures/${fixtureName}`, import.meta.url),
+    to: new URL("./root/index.html", import.meta.url),
+    overwrite: true,
+  });
+  await writeImportmaps({
+    logLevel: "warn",
+    projectDirectoryUrl: testDirectoryUrl,
+    importmaps: {
+      "./index.html": {
+        mappingsForNodeResolution: true,
+        removeUnusedMappings: true,
+      },
+    },
+  });
+  copyFileSync({
+    from: new URL("./root/index.html", import.meta.url),
+    to: new URL(`./snapshots/${fixtureName}`, import.meta.url),
+    overwrite: true,
+  });
+};
+
+const directorySnapshot = takeDirectorySnapshot(snapshotDirectoryUrl);
+await test("index_importmap_empty.html");
+await test("index_importmap_with_src.html");
+await test("index_without_importmap.html");
+directorySnapshot.compare();
