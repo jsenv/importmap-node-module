fbd5b4132382ce2f6bd2b5f4143319d3ced83edf
dmail <dmaillard06@gmail.com>
2023-11-28 15:07:05 +0100

    prepare injecting importmap to html files

--- src/step_entry_point/html_parser.js
+++ src/step_entry_point/html_parser.js
@@ -1,52 +1,63 @@
-import { createRequire } from "node:module";
 import { readFile } from "@jsenv/filesystem";
-import { urlToFilename } from "@jsenv/urls";
-
-const require = createRequire(import.meta.url);
+import {
+  parseHtml,
+  visitHtmlNodes,
+  getHtmlNodeAttribute,
+  getHtmlNodeText,
+  generateUrlForInlineContent,
+  getHtmlNodeAttributePosition,
+  getHtmlNodePosition,
+} from "@jsenv/ast";
 
 export const parseHTMLRessources = async ({ code, url, asFileUrl }) => {
-  const parse5 = require("parse5");
-  const htmlAst = parse5.parse(code, { sourceCodeLocationInfo: true });
+  const htmlAst = parseHtml({ html: code, url, storeOriginalPositions: false });
   const htmlRessources = [];
-  visitHtmlAst(htmlAst, (node) => {
-    if (node.nodeName !== "script") {
-      return;
-    }
+  visitHtmlNodes(htmlAst, {
+    script: (node) => {
+      const type = getHtmlNodeAttribute(node, "type");
+      if (type !== "module") {
+        return;
+      }
+      const src = getHtmlNodeAttribute(node, "src");
+      if (src) {
+        const { line, column } = getHtmlNodeAttributePosition(node, "src");
+        htmlRessources.push({
+          type: "module script",
+          url: new URL(src, url).href,
+          // expectedContentType: "application/javascript",
+          line,
+          column,
+        });
+        return;
+      }
 
-    const typeAttribute = getHtmlNodeAttributeByName(node, "type");
-    const type = typeAttribute ? typeAttribute.value : "application/javascript";
-    if (type !== "module") {
-      return;
-    }
-
-    const srcAttribute = getHtmlNodeAttributeByName(node, "src");
-    const src = srcAttribute ? srcAttribute.value : "";
-    if (src) {
-      htmlRessources.push({
-        type: "module script",
-        url: new URL(src, url).href,
-        // expectedContentType: "application/javascript",
-        ...getHtmlNodeLocation(node, "src"),
+      const text = getHtmlNodeText(node);
+      const { line, column, lineEnd, columnEnd } = getHtmlNodePosition(node, {
+        preferOriginal: true,
       });
-      return;
-    }
-
-    const textNode = getHtmlNodeTextNode(node);
-    const id = getIdForInlineHtmlNode(node, htmlAst);
-    const specifier = `${urlToFilename(url)}__inline__${id}.js`;
-    htmlRessources.push({
-      type: "inline module script",
-      url: new URL(specifier, url).href,
-      // expectedContentType: "application/javascript",
-      ...getHtmlNodeLocation(node),
-      content: textNode.value,
-      contentType: "application/javascript",
-    });
+      const urlForInlineContent = generateUrlForInlineContent({
+        url,
+        extension: ".js",
+        line,
+        column,
+        lineEnd,
+        columnEnd,
+      });
+      htmlRessources.push({
+        type: "inline module script",
+        url: urlForInlineContent,
+        // expectedContentType: "application/javascript",
+        line,
+        column,
+        content: text,
+        contentType: "application/javascript",
+      });
+    },
   });
-  await htmlRessources.reduce(async (previous, htmlRessource) => {
-    await previous;
+
+  for (const htmlRessource of htmlRessources) {
     if (htmlRessource.type !== "module script") {
-      return;
+      continue;
     }
     // let's fetch the url
     const fileUrl = asFileUrl(htmlRessource.url);
@@ -57,82 +68,6 @@ export const parseHTMLRessources = async ({ code, url, asFileUrl }) => {
     } else {
       htmlRessource.isExternal = true;
     }
-  }, Promise.resolve());
+  }
   return htmlRessources;
-};
-
-const STOP_AST_VISIT = "stop";
-const visitHtmlAst = (htmlAst, callback) => {
-  const visitNode = (node) => {
-    const callbackReturnValue = callback(node);
-    if (callbackReturnValue === STOP_AST_VISIT) {
-      return;
-    }
-    const { childNodes } = node;
-    if (childNodes) {
-      let i = 0;
-      while (i < childNodes.length) {
-        visitNode(childNodes[i++]);
-      }
-    }
-  };
-  visitNode(htmlAst);
-};
-
-const getHtmlNodeAttributeByName = (htmlNode, attributeName) => {
-  const attrs = htmlNode.attrs;
-  return attrs && attrs.find((attr) => attr.name === attributeName);
-};
-
-const getHtmlNodeTextNode = (htmlNode) => {
-  const firstChild = htmlNode.childNodes[0];
-  return firstChild && firstChild.nodeName === "#text" ? firstChild : null;
-};
-
-const getHtmlNodeLocation = (htmlNode, htmlAttributeName) => {
-  const { sourceCodeLocation } = htmlNode;
-  if (!sourceCodeLocation) {
-    return null;
-  }
-
-  if (!htmlAttributeName) {
-    const { startLine, startCol } = sourceCodeLocation;
-    return {
-      line: startLine,
-      column: startCol,
-    };
-  }
-
-  const attributeSourceCodeLocation =
-    sourceCodeLocation.attrs[htmlAttributeName];
-  if (!attributeSourceCodeLocation) {
-    return null;
-  }
-  const { startLine, startCol } = attributeSourceCodeLocation;
-  return {
-    line: startLine,
-    column: startCol,
-  };
-};
-
-const getIdForInlineHtmlNode = (node, htmlAst) => {
-  const idAttribute = getHtmlNodeAttributeByName(node, "id");
-  if (idAttribute) {
-    return idAttribute.value;
-  }
-  const { line, column } = getHtmlNodeLocation(node) || {};
-  let lineTaken = false;
-  visitHtmlAst(htmlAst, (nodeCandidate) => {
-    if (nodeCandidate === node) return null;
-    if (nodeCandidate.nodeName === "#text") return null;
-    const htmlNodeLocation = getHtmlNodeLocation(nodeCandidate);
-    if (!htmlNodeLocation) return null;
-    if (htmlNodeLocation.line !== line) return null;
-    lineTaken = true;
-    return STOP_AST_VISIT;
-  });
-  if (lineTaken) {
-    return `${line}.${column}`;
-  }
-  return line;
 };
--- src/step_write_into_files/write_into_files.js
+++ src/step_write_into_files/write_into_files.js
@@ -1,5 +1,15 @@
-import { writeFileSync } from "@jsenv/filesystem";
+import { readFileSync, writeFileSync } from "@jsenv/filesystem";
 import { urlToFileSystemPath } from "@jsenv/urls";
+import {
+  parseHtml,
+  findHtmlNode,
+  getHtmlNodeAttribute,
+  setHtmlNodeAttributes,
+  setHtmlNodeText,
+  createHtmlNode,
+  injectHtmlNodeAsEarlyAsPossible,
+  stringifyHtmlAst,
+} from "@jsenv/ast";
 
 export const writeIntoFiles = (
   importmapInfos,
@@ -7,9 +17,49 @@ export const writeIntoFiles = (
 ) => {
   for (const importmapRelativeUrl of Object.keys(importmapInfos)) {
     const importmapInfo = importmapInfos[importmapRelativeUrl];
-    const importmapUrl = new URL(importmapRelativeUrl, projectDirectoryUrl);
     const importmap = importmapInfo.importmap;
-    writeFileSync(importmapUrl, JSON.stringify(importmap, null, "  "));
-    logger.info(`-> ${urlToFileSystemPath(importmapUrl)}`);
+    const fileUrl = new URL(importmapRelativeUrl, projectDirectoryUrl).href;
+    const importmapAsJson = JSON.stringify(importmap, null, "  ");
+    if (fileUrl.endsWith(".html")) {
+      writeIntoHtmlFile(fileUrl, importmapAsJson, { logger });
+    } else {
+      writeFileSync(fileUrl, importmapAsJson);
+      logger.info(`-> ${urlToFileSystemPath(fileUrl)}`);
+    }
   }
+};
+
+const writeIntoHtmlFile = (htmlFileUrl, importmapAsJson, { logger }) => {
+  const htmlAst = parseHtml({
+    html: readFileSync(htmlFileUrl, "utf8"),
+    url: htmlFileUrl,
+    storeOriginalPositions: false,
+  });
+  const scriptTypeImportmap = findHtmlNode(htmlAst, (node) => {
+    if (node.nodeName !== "script") {
+      return false;
+    }
+    const typeAttribute = getHtmlNodeAttribute(node, "type");
+    return typeAttribute === "importmap";
+  });
+
+  if (scriptTypeImportmap) {
+    const srcAttribute = getHtmlNodeAttribute(scriptTypeImportmap, "src");
+    if (srcAttribute) {
+      setHtmlNodeAttributes(scriptTypeImportmap, { src: undefined });
+      logger.warn(`remove src=${srcAttribute} from <script type="module">`);
+    }
+    setHtmlNodeText(scriptTypeImportmap, importmapAsJson);
+  } else {
+    injectHtmlNodeAsEarlyAsPossible(
+      htmlAst,
+      createHtmlNode({
+        tagName: "script",
+        type: "importmap",
+        textContent: importmapAsJson,
+      }),
+    );
+  }
+  const html = stringifyHtmlAst(htmlAst);
+  writeFileSync(htmlFileUrl, html);
 };
