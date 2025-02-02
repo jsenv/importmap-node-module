3aa9ab97bc1ca795c62a7767a4c84cbbbb09eff7
dmail <dmaillard06@gmail.com>
2025-02-02 10:57:47 +0100

    update snapshot

--- tests/entry_point_check/entry_point_many_2/entry_point_many_2.test.mjs
+++ tests/entry_point_check/entry_point_many_2/entry_point_many_2.test.mjs
@@ -2,11 +2,12 @@ import { replaceFileStructureSync } from "@jsenv/filesystem";
 import { writeImportmaps } from "@jsenv/importmap-node-module";
 import { snapshotWriteImportmaps } from "@jsenv/importmap-node-module/tests/snapshot_write_importmaps.js";
 
+replaceFileStructureSync({
+  from: new URL("./fixtures/", import.meta.url),
+  to: new URL("./git_ignored/", import.meta.url),
+});
+
 const run = async () => {
-  replaceFileStructureSync({
-    from: new URL("./fixtures/", import.meta.url),
-    to: new URL("./git_ignored/", import.meta.url),
-  });
   await writeImportmaps({
     logLevel: "warn",
     directoryUrl: new URL("./git_ignored/", import.meta.url),
