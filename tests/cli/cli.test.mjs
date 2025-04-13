2d20c70d79db4d5045589eee9c03fb3c4289a89c
dmail <dmaillard06@gmail.com>
2025-04-13 05:42:00 +0200

    prepare cli test

--- tests/cli/cli.test.mjs
+++ tests/cli/cli.test.mjs
@@ -1,3 +1,9 @@
 import { execSync } from 'node:child_process'
+import { takeFileSnapshot } from '@jsenv/snapshot'
 
-execSync('node ../../src/cli.mjs ../tests/cli/client/index.html')
+const indexHtmlFileUrl = import.meta.resolve('./client/index.html')
+const indexHtmlFileSnapshot = takeFileSnapshot(indexHtmlFileUrl)
+
+execSync('node ../../../src/cli.mjs ./index.html', { cwd: new URL(import.meta.resolve('./client/')) })
+
+indexHtmlFileSnapshot.compare()
