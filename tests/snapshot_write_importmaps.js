bbb91142bbd4b430dd3d42507a3aa3ecea55d167
dmail <dmaillard06@gmail.com>
2025-06-02 21:09:58 +0200

    update snapshots

--- tests/snapshot_write_importmaps.js
+++ tests/snapshot_write_importmaps.js
@@ -1,6 +1,10 @@
 import { snapshotTests } from "@jsenv/snapshot";
 
-export const snapshotWriteImportmaps = (url, fnRegisteringTests, options) => {
+export const snapshotWriteImportmaps = (
+  url,
+  fnRegisteringTests,
+  options = {},
+) => {
   return snapshotTests(url, fnRegisteringTests, {
     filesystemEffects: {
       textualFilesInline: true,
