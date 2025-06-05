ce7a68cd2dd28a0726e02b085a6fa71083f728b9
dmail <dmaillard06@gmail.com>
2025-06-05 15:35:26 +0200

    prepare cli test

---
+++ tests/cli/basic/cli_basic.test.mjs
@@ -0,0 +1,19 @@
+import { copyFileSync, replaceFileStructureSync } from "@jsenv/filesystem";
+import { takeFileSnapshot } from "@jsenv/snapshot";
+import { execSync } from "node:child_process";
+
+const indexHtmlFileSnapshot = takeFileSnapshot(
+  import.meta.resolve("./snapshots/index.html"),
+);
+replaceFileStructureSync({
+  from: import.meta.resolve("./fixtures/"),
+  to: import.meta.resolve("./git_ignored/"),
+});
+execSync("node ../../../../src/cli.mjs ./index.html", {
+  cwd: new URL(import.meta.resolve("./git_ignored/")),
+});
+copyFileSync({
+  from: import.meta.resolve("./git_ignored/index.html"),
+  to: import.meta.resolve("./snapshots/index.html"),
+});
+indexHtmlFileSnapshot.compare();
